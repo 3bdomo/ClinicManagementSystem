f47fedbbfbee6d6745fb2e06b6fef9c850e1b028
amral6ss <am2873328@gmail.com>
2026-05-01 00:23:00 +0200

    Initialize DAL

---
+++ Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IPatientRepository Patients {get; }
+        IDoctorRepository Doctors {get; }
+        IDoctorScheduleRepository DoctorSchedules {get; }
+        IAppointmentRepository Appointments {get; }
+        IMedicalRecordRepository MedicalRecords {get; }
+        IProcedureTypeRepository ProcedureTypes {get; }
+        IProcedureRepository Procedures {get; }
+        IInvoiceRepository Invoices {get; }
+        Task SaveChangesAsync();    
+    }
+}
