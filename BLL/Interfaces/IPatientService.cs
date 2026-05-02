5bfad3e177664825a412c4c383d7b9891a39de4b
amral6ss <am2873328@gmail.com>
2026-05-02 21:02:49 +0200

    Setup Layer BLL

---
+++ BLL/Interfaces/IPatientService.cs
@@ -0,0 +1,22 @@
+﻿using BLL.DTOs.Patient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Results;
+namespace BLL.Interfaces
+{
+    public interface IPatientService
+    {
+        Task<OperationResult<IEnumerable<PatientDTO>>> GetAllAsync(int page, int pageSize);
+        Task<OperationResult<PatientDTO>> GetByIdAsync(int id);
+        Task<OperationResult<IEnumerable<PatientDTO>>> SearchAsync(string query);
+        Task<OperationResult> CreateAsync(PatientDTO dto);
+        Task<OperationResult> UpdateAsync(PatientDTO dto);
+        Task<OperationResult> DeleteAsync(int id);         // soft delete
+        Task<OperationResult<PatientHistoryDto>> GetFullHistoryAsync(int id);
+        Task<OperationResult<IEnumerable<PatientDTO>>> GetDeletedAsync();// ✅ Admin only
+        Task<OperationResult> RestoreAsync(int id);     //     ✅ Admin only
+    }
+}
---
+++ BLL/Services/PatientService.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using BLL.DTOs.Patient;
+using BLL.Interfaces;
+using Common.Results;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    internal class PatientService : IPatientService
+    {
+      
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public PatientService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<OperationResult> CreateAsync(PatientDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<IEnumerable<PatientDTO>>> GetAllAsync(int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<PatientDTO>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<IEnumerable<PatientDTO>>> GetDeletedAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<PatientHistoryDto>> GetFullHistoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> RestoreAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<IEnumerable<PatientDTO>>> SearchAsync(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> UpdateAsync(PatientDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
