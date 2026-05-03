5320a2eea753d40d6005c74d050bb8232d652ecf
RomanyMalak <romany.malak.kamel.sabet@gmail.com>
2026-05-03 17:01:26 +0300

    Update in home views

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,27 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using UI.Models;
 
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+public class HomeController : Controller
+{
+    public IActionResult Index() => View();
 }
