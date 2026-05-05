949b4c32e9c7668589b36a07cfa0b41a0dba96a2
amral6ss <am2873328@gmail.com>
2026-05-05 22:55:30 +0200

    Add Services

--- BLL/Extensions/BllServiceExtensions.cs
+++ BLL/Extensions/BllServiceExtensions.cs
@@ -1,5 +1,6 @@
 using BLL.Interfaces;
 using BLL.Services;
+using BLL.Services.Implementations;
 using ClinicSystem.BLL.Mapping;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,6 +14,34 @@ public static class BllServiceExtensions
         services.AddAutoMapper(typeof(MappingProfile).Assembly);
 
         services.AddScoped<IPatientService, PatientService>();
+
+        services.AddScoped<IDoctorService, DoctorService>();
+
+        //services.AddScoped<IAppointmentService, AppointmentService>();
+
+        services.AddScoped<IBillingService, BillingService>();
+
+
+        services.AddScoped<IMedicalRecordService, MedicalRecordService>();
+
+        services.AddScoped<IAuthService, AuthService>();
+
+
+        services.AddScoped<IMedicalRecordService, MedicalRecordService>();
+
+        services.AddScoped<IDoctorScheduleService, DoctorScheduleService>();
+
+        services.AddScoped <IPatientAccountService, PatientAccountService>();
+
+        //services.AddScoped <IDashboardService, DashboardService>();
+
+        services.AddScoped <IReceptionistService , ReceptionistService>();
+
+        services.AddScoped <IUserService, UserService>();
+
+
+
+
 
         return services;
     }
