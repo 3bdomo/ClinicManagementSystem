000bdd410a99d54e4c81597aae4a13af84caa746
RomanyMalak <romany.malak.kamel.sabet@gmail.com>
2026-05-06 10:43:54 +0300

    update

--- Web/Controllers/DoctorController.cs
+++ Web/Controllers/DoctorController.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+[Authorize(Roles = "Admin")]
 public class DoctorController : Controller
 {
     public IActionResult Index() => View();
